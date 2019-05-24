a2cc5f18d5a21018a8fe5eb5dc07ad99f8be0a02
Mubashar Ahmad <zanysoft@outlook.com>
2019-05-24 12:24:54 +0500

    Fixing issues

--- src/Models/MenuItems.php
+++ src/Models/MenuItems.php
@@ -7,22 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 class MenuItems extends Model
 {
 
-	protected $table = null;
+    protected $table = null;
 
-	public function __construct( array $attributes = [] ){
-    	//parent::construct( $attributes );
-    	$this->table = config('menu.table_prefix') . config('menu.table_name_items');
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+        $this->table = config('menu.table_prefix') . config('menu.table_name_items');
     }
 
-    public function getsons($id) {
-		return $this -> where("parent", $id) -> get();
-	}
-	public function getall($id) {
-		return $this -> where("menu", $id) -> orderBy("sort", "asc")->get();
-	}
+    public function getsons($id)
+    {
+        return $this->where("parent", $id)->get();
+    }
 
-	public static function getNextSortRoot($menu){
-        return self::where('menu',$menu)->where('depth',0)->max('sort') + 1;
+    public function getall($id)
+    {
+        return $this->where("menu", $id)->orderBy("sort", "asc")->get();
+    }
+
+    public static function getNextSortRoot($menu)
+    {
+        return self::where('menu', $menu)->where('depth', 0)->max('sort') + 1;
     }
 
 }
